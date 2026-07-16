b30564478dc8c80a47741326a954d73156c83f82
Emilie Athanas <emilieathanasenas@gmail.com>
2026-07-15 20:22:49 -0400

    feat: basic navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,6 +4,10 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
+        <div className="nav-logo">
+        <h2>{`</EA>`}</h2>
+        </div>
+        <div className="nav-links">
           <a className="nav-link" href="/projects">
             Projects
           </a>
@@ -13,6 +17,7 @@ export default function Navbar() {
           <a className="nav-link" href="/contact">
             Contact
           </a>
+          </div>
         </div>
     </nav>
   )
