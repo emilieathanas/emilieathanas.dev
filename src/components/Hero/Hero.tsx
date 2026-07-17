9fb2539cb285df3c4af07d2964bce629f9509faa
Emilie Athanas <emilieathanasenas@gmail.com>
2026-07-16 21:56:16 -0400

    feat: start hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import './Hero.css'
+export default function Hero() {
+    return (
+        <section className="hero">
+            <h1 className="welcome-heading">Hi, I'm Emilie</h1>
+            <p className="text-lg">I am a Computer Science student.</p>
+        </section>
+    )
+
+
+
+}
