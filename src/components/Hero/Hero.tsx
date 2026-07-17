49de8ac381911e77d99a2ff768d9eda444c33b8a
Emilie Athanas <emilieathanasenas@gmail.com>
2026-07-17 19:20:49 -0400

    fix: change hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
   return (
     <section className="hero">
       <h1 className="welcome-heading">Hi, I'm Emilie</h1>
-      <p className="text-lg">I am a Computer Science student.</p>
+      <p className="text-lg">I am a Computer Science student with a passion for web development.</p>
     </section>
   )
 }
